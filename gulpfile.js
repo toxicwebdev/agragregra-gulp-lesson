0155237964fe11ba2a6b593d602be357a80d33ba
Алексей <agragregra@ya.ru>
2016-03-10 14:57:08 +0700

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-var gulp      = require('gulp'), // Подключаем Gulp
-	sass        = require('gulp-sass'), //Подключаем Sass пакет,
-	browserSync = require('browser-sync'), // Подключаем Browser Sync
-	concat      = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-	uglify      = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
-	cssnano     = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
-	rename      = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-	del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
-	imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-	pngquant    = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-	cache       = require('gulp-cache'); // Подключаем библиотеку кеширования
+var gulp       = require('gulp'), // Подключаем Gulp
+	sass         = require('gulp-sass'), //Подключаем Sass пакет,
+	browserSync  = require('browser-sync'), // Подключаем Browser Sync
+	concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+	uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+	cssnano      = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+	rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+	del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
+	imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+	pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+	cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function(){ // Создаем таск Sass
 	return gulp.src('app/sass/**/*.sass') // Берем источник
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
