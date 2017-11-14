97ef213a77d504bb5e184eef469d8318857ec355
Алексей <agragregra@ya.ru>
2017-11-14 18:43:05 +0700

    gulp-cache-update

--- gulpfile.js
+++ gulpfile.js
@@ -57,13 +57,13 @@ gulp.task('clean', function() {
 
 gulp.task('img', function() {
 	return gulp.src('app/img/**/*') // Берем все изображения из app
-		// .pipe(cache(imagemin({ // С кешированием
-		.pipe(imagemin({  // Сжимаем изображения без кеширования
+		.pipe(cache(imagemin({ // С кешированием
+		// .pipe(imagemin({ // Сжимаем изображения без кеширования
 			interlaced: true,
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
 			use: [pngquant()]
-		}))
+		}))/**/)
 		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
 });
 
